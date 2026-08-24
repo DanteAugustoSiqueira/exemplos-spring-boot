cdab1172e558c5cf3c96ae66febcf0b05b62dd1f
Dante Augusto <167940415+DanteAugustoSiqueira@users.noreply.github.com>
2026-08-24 10:20:35 -0300

    Add files via upload

---
+++ src/test/java/br/senac/tads/dsw/exemplos_spring_boot/ExemplosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.exemplos_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplosSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
